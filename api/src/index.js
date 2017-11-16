0a8a781ccaee5c18f23f75294c6ec28b9e7ad7d5
Josh Gretz <jgretz@truefit.io>
2017-11-16 10:47:49 -0500

    get basic reader working

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,22 @@
 import nodeBits from 'node-bits';
-import nodeBitsExpress from 'node-bits-express';
+import nodeBitsExpress, {cors, bodyParser, accessControl} from 'node-bits-express';
 import nodeBitsCode from 'node-bits-code';
+
+import express from 'express';
+import path from 'path';
 
 nodeBits([
   nodeBitsExpress({
     port: 4004,
+    configurations: [
+      accessControl(),
+      cors(),
+      bodyParser({}),
+
+      app => {
+        app.use('/books', express.static(path.join(__dirname, 'books')));
+      },
+    ],
   }),
   nodeBitsCode({
     path: `${__dirname}`,
--- site/app/features/shared/components/landing.js
+++ site/app/features/shared/components/landing.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {ReactReader} from 'react-reader';
 
-export default () => (
-  <div>Hello</div>
-);
+// https://s3-eu-west-1.amazonaws.com/react-reader/alice.epub
+
+export default class Landing extends Component {
+  render() {
+    return (
+      <div style={{position: 'relative', height: '100%'}}>
+        <ReactReader
+          url={'http://localhost:4004/books/life-on-the-mississippi.epub'}
+          title={'Test'} />
+      </div>
+    );
+  }
+}
